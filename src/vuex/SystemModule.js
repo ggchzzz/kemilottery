603c679048e73449daba2cb37105eb96d21a74e2
ggchzzz <ggchzzz@163.com>
2019-01-22 09:36:16 +0800

    SystemModule

--- src/vuex/SystemModule.js
+++ src/vuex/SystemModule.js
@@ -2,16 +2,23 @@ import axios from '@/tools/axios'
 import api from '@/apis'
 import { stat } from 'fs';
 const state={
-    allUsers:[]
+    allUsers:[],
+    allRolles:[]
 }
 const getters={
         allUsers(state){
             return state.allUsers;
+        },
+        allRolles(state){
+            return
         }
 }
 const mutations={
     loadAllACCOUNT(state,payload){
     state.allUsers=payload.allUsers;
+    },
+    LOADALLROLES(){
+        state.allRolles=payload.allRolles;
     }
 }
 const actions={
@@ -23,6 +30,12 @@ const actions={
         // commit("loadAllACCOUNT")
         // axios.get(payload.url,payload.params)
    
+    },
+    loadAllRoles({commit},payload){
+        axios.get(api.findAllUsers).then(resp=>{
+            console.log(resp);
+            commit("loadAllACCOUNT",resp.data)
+        })
     }
 }
 export default{
